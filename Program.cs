562aee6ae5009cded6578d318e597dcff445367f
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:41:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _78DersEnums //Haftanin gunleri gibi belirli veya belirsiz ogeleri indexleyerek tutmaya yarar...
+{
+    enum Gunler {Pazartesi=1,Sali,Carsamba,Persembe,Cuma,Cumartesi,Pazar}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)Gunler.Pazartesi);
+            Console.ReadLine();
+        }
+    }
+}
